ad2e0c35f9e126c94d59242b27d250a4dd38cbfc
theperudev <112427227+theperudev@users.noreply.github.com>
2022-08-30 00:36:50 +0530

    Create jsquest.js

---
+++ jsquest.js
@@ -0,0 +1,15 @@
+var questions =[
+    {
+        num : 1,
+        question : "What does HTML stand for?",
+        answer : "Hyper Text Markup Language",
+        options : [
+            "Hyper Text Multiple Language",
+            "Hyper Text Preprocessor",
+            "Hyper Tool Multi Language",
+            "Hyper Text Markup Language"
+        ]
+    },
+    
+
+];
