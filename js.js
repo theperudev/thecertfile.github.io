39340f24f6ead1455228b922395f8f6e93ee9232
theperudev <112427227+theperudev@users.noreply.github.com>
2022-09-05 17:22:04 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,29 @@
-alert('hola')
+
+var asking =[
+    {
+        num : 1,
+        ok : thekey.doitnow('V2hhdCBkb2VzIEhUTUwgc3RhbmQgZm9yPw=='),
+        wr : "Hyper Text Markup Language",
+        ch : [
+            "Hyper Text Multiple Language",
+            "Hyper Text Preprocessor",
+            "Hyper Tool Multi Language",
+            "Hyper Text Markup Language"
+        ]
+    },
+
+
+    {
+        num : 2,
+        ok : 'ABCD',
+        wr : "Hyper Text Markup Language",
+        ch : [
+            "Hyper Text Multiple Language",
+            "Hyper Text Preprocessor",
+            "Hyper Tool Multi Language",
+            "Hyper Text Markup Language"
+        ]
+    },
+    
+
+];
