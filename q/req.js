cc000ebc7ad56796032557fbe8ff64e09dea3e81
theperudev <112427227+theperudev@users.noreply.github.com>
2022-09-06 13:18:32 +0530

    Update req.js

--- q/req.js
+++ q/req.js
@@ -35,50 +35,7 @@ linkedin1+='='+years+'&issueMonth='+monthss+'&certUrl=';
 
 var linkedin2='&certId=';
 
-
-const config = {
-  apiKey: "AIzaSyA_QH5DQBtgf4wiGNS1pPdnqjlHh9ck418",
-  authDomain: "linkedinquiz-826d1.firebaseapp.com",
-  projectId: "linkedinquiz-826d1",
-  storageBucket: "linkedinquiz-826d1.appspot.com",
-  messagingSenderId: "801022372497",
-  appId: "1:801022372497:web:864d999f478e50516c2d30",
-  measurementId: "G-7D7SGDW6EJ"
-};
-
-firebase.initializeApp(config);
-const auth = firebase.auth();
-
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
- 
-   }
-  return result
-}
-
-function makeidd(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
- 
-   }
-   return result
-}
-
-
-function getid()
-{
-    const myid=makeid(6)+"-"+makeidd(5)+"-"+cname+"-ND";
-   return myid
-}
+var _0x171abc=_0x2d4b;(function(_0xcf3621,_0x5ec72f){var _0x5226a6=_0x2d4b,_0x2f9379=_0xcf3621();while(!![]){try{var _0x542bad=parseInt(_0x5226a6(0xf9))/0x1+parseInt(_0x5226a6(0x119))/0x2+-parseInt(_0x5226a6(0x101))/0x3*(parseInt(_0x5226a6(0x104))/0x4)+-parseInt(_0x5226a6(0x109))/0x5+parseInt(_0x5226a6(0x112))/0x6*(parseInt(_0x5226a6(0x113))/0x7)+-parseInt(_0x5226a6(0xfe))/0x8+parseInt(_0x5226a6(0x117))/0x9*(parseInt(_0x5226a6(0x107))/0xa);if(_0x542bad===_0x5ec72f)break;else _0x2f9379['push'](_0x2f9379['shift']());}catch(_0x102dd0){_0x2f9379['push'](_0x2f9379['shift']());}}}(_0x3df1,0x4ac0c));const _0x2e915d={'apiKey':_0x171abc(0x100)+'5DQBtgf4wi'+_0x171abc(0x116)+'lHh9ck418','authDomain':'linkedinqu'+_0x171abc(0xfc)+_0x171abc(0xfb)+_0x171abc(0x108),'projectId':'linkedinqu'+'iz-826d1','storageBucket':_0x171abc(0x10d)+_0x171abc(0xff)+'ppspot.com','messagingSenderId':_0x171abc(0x110)+'97','appId':_0x171abc(0x115)+_0x171abc(0xfd)+_0x171abc(0x10a)+_0x171abc(0x103)+'0','measurementId':_0x171abc(0x10f)+'EJ'};function _0x2d4b(_0x33f06e,_0x523938){var _0x3df17d=_0x3df1();return _0x2d4b=function(_0x2d4b17,_0x26bb8b){_0x2d4b17=_0x2d4b17-0xf7;var _0x27a5f7=_0x3df17d[_0x2d4b17];return _0x27a5f7;},_0x2d4b(_0x33f06e,_0x523938);}firebase[_0x171abc(0xf7)+_0x171abc(0x10c)](_0x2e915d);const _0x27b36d=firebase[_0x171abc(0xfa)]();function _0x4c370f(_0x3d9555){var _0x2627b3=_0x171abc,_0x392081='',_0x1bc1c2=_0x2627b3(0x106)+_0x2627b3(0x114)+_0x2627b3(0x118)+_0x2627b3(0x10e),_0x2e2383=_0x1bc1c2[_0x2627b3(0x11a)];for(var _0x262e0d=0x0;_0x262e0d<_0x3d9555;_0x262e0d++){_0x392081+=_0x1bc1c2[_0x2627b3(0x111)](Math[_0x2627b3(0xf8)](Math['random']()*_0x2e2383));}return _0x392081;}function _0x182e97(_0xdbbe6){var _0x5b9e5b=_0x171abc,_0xa80469='',_0x3e75e2='ABCDEFGHIJ'+_0x5b9e5b(0x114)+_0x5b9e5b(0x118)+'456789',_0x405d46=_0x3e75e2['length'];for(var _0x4c6c13=0x0;_0x4c6c13<_0xdbbe6;_0x4c6c13++){_0x5b9e5b(0x102)==='vLKVv'?_0xa80469+=_0x3e75e2[_0x5b9e5b(0x111)](Math['floor'](Math[_0x5b9e5b(0x10b)]()*_0x405d46)):_0x77b7a0+=_0x139c1d[_0x5b9e5b(0x111)](_0x1d91d9[_0x5b9e5b(0xf8)](_0x99dd60[_0x5b9e5b(0x10b)]()*_0xc26f45));}return _0xa80469;}function _0x3df1(){var _0x2a97f3=['ABCDEFGHIJ','480qqBMzs','.com','2204325nWyvcV','64d999f478','random','App','linkedinqu','456789','G-7D7SGDW6','8010223724','charAt','12clGNBq','286153QPpMYz','KLMNOPQRST','1:80102237','GNS1pPdnqj','181458UZfonE','UVWXYZ0123','191378cItYQM','length','initialize','floor','339460iuVxlx','auth','irebaseapp','iz-826d1.f','2497:web:8','3319184dJbOCV','iz-826d1.a','AIzaSyA_QH','3BvNceb','vLKVv','e50516c2d3','1290928EEQSvF','-ND'];_0x3df1=function(){return _0x2a97f3;};return _0x3df1();}function _0x1dde84(){var _0x58b1cd=_0x171abc;const _0x52c985=_0x4c370f(0x6)+'-'+_0x182e97(0x5)+'-'+cname+_0x58b1cd(0x105);return _0x52c985;}
 
 
 start_btn.onclick =()=>{
@@ -123,104 +80,7 @@ var AllOptions = options_box.querySelectorAll(".option");
     }
     next_btn.classList.add("inactive");
 }
-
-
-next_btn.onclick=()=>{
-    que_index++;
-    
-    if(asking.length>que_index){
-        count_que.innerText = que_index+1;
-        ShowQuestion(que_index);
-    }else{
-         var linkedinimg=document.getElementById('linkedinimg');
-         percentage.innerText = ((right_answers*100)/asking.length).toFixed(2);
-
-        if (percentage.innerText>45)
-        {
-           // console.log("asking Complete");
-        quiz_box.classList.add("inactive");
-        result_box.classList.remove("inactive");
-        right_ans_r.innerText = right_answers;
-        wrong_ans_r.innerText = wrong_answers;
-        const theidofcert=getid();
-        var ilink='https://validate.narendradwivedi.org/?certificate='+theidofcert+'&c='+cname;
-
-        var ilink1='https://validate.narendradwivedi.org/?certificate%3D'+theidofcert+'%26c%3D'+cname;
-        //document.getElementById('certid').innerHTML='Your URL Of Quiz Completion Is <a href="https://validate.narendradwivedi.org/?certificate='+theidofcert+'+'>'+'Hola</a>';
-        document.getElementById('certid').style.display="block";
-        document.getElementById('thelink').href=ilink;
-        document.getElementById('thelink').innerText=ilink;
-        
-        document.getElementById('sharetext').innerText='Use Below Button To Add Your Certificate To LinkedIn';
-
-        document.getElementById('credit').innerHTML='Make Sure To Follow <a href="https://www.linkedin.com/in/narendradwivedi">Narendra Dwivedi</a> And <a href="https://www.linkedin.com/company/narendradwivedi">NarendraDwivedi.Org</a> On LinkedIn';
-
-        document.getElementById('linkedinlink').href=linkedin1+ilink1+linkedin2+theidofcert+encodeURIComponent('&c=')+cname;
-         
-          linkedinimg.src='https://i.ibb.co/j80w7tX/linkedin.png';
-
-          // saveit
-
-          const id = theidofcert;
-    const myname = document.getElementById("username").value;
-
-
-    var today = new Date();
-
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-var dateTime = date+' '+time;
-
- 
-var thelevel=document.getElementById('level');
-
-var levelit='';
-if (thelevel.innerText='b')
-{
-    levelit='Beginner';
-}
-else if (thelevel.innerText='i')
-{
-    levelit='Intermediate';
-}
-
-else if (thelevel.innerText='a')
-{
-    levelit='Advance';
-}
-
-else
-{
-    levelit='Normal';
-}
-
-        firebase.database().ref(cname+'/' + id).set({
-            ID: id,
-            Name: myname,
-            Difficulty:levelit,
-            Quiz:thetitle,
-            Date: date,
-            Time:time
-        });
-        //alert("Successfully Submitted");
-         
-        }
-        else
-        {
-            alert('Your Marks Is Not 45%+ , Please Attempt After Learning');
-            window.location='https://quiztest.narendradwivedi.org';
-            
-        }
-
-        
-    }
-
-    if(asking.length-1==que_index){
-        next_btn.innerText = "Finish";
-    }
-}
+var _0x27720e=_0x261d;function _0x261d(_0x1bbe1d,_0xae868a){var _0x224a41=_0x224a();return _0x261d=function(_0x261de9,_0x338b43){_0x261de9=_0x261de9-0x19a;var _0x310347=_0x224a41[_0x261de9];return _0x310347;},_0x261d(_0x1bbe1d,_0xae868a);}(function(_0x226c42,_0x233b40){var _0x48290f=_0x261d,_0x2dff52=_0x226c42();while(!![]){try{var _0x383960=-parseInt(_0x48290f(0x1b0))/0x1*(parseInt(_0x48290f(0x1a3))/0x2)+-parseInt(_0x48290f(0x1bc))/0x3*(parseInt(_0x48290f(0x1d7))/0x4)+parseInt(_0x48290f(0x19f))/0x5*(parseInt(_0x48290f(0x1bd))/0x6)+parseInt(_0x48290f(0x1b4))/0x7+parseInt(_0x48290f(0x1b1))/0x8+parseInt(_0x48290f(0x1dd))/0x9+-parseInt(_0x48290f(0x1cf))/0xa;if(_0x383960===_0x233b40)break;else _0x2dff52['push'](_0x2dff52['shift']());}catch(_0x43a71b){_0x2dff52['push'](_0x2dff52['shift']());}}}(_0x224a,0xaf04f),next_btn[_0x27720e(0x1ad)]=()=>{var _0x3f6ebd=_0x27720e;que_index++;if(asking['length']>que_index)count_que[_0x3f6ebd(0x1e1)]=que_index+0x1,ShowQuestion(que_index);else{var _0x345103=document[_0x3f6ebd(0x1ca)+_0x3f6ebd(0x1e0)](_0x3f6ebd(0x19a)+'g');percentage[_0x3f6ebd(0x1e1)]=(right_answers*0x64/asking[_0x3f6ebd(0x1da)])[_0x3f6ebd(0x1bf)](0x2);if(percentage[_0x3f6ebd(0x1e1)]>0x2d){if(_0x3f6ebd(0x1d3)!==_0x3f6ebd(0x1d3))_0x3aec75=_0x3f6ebd(0x1a9)+'te';else{quiz_box[_0x3f6ebd(0x1cd)][_0x3f6ebd(0x1a4)](_0x3f6ebd(0x1c1)),result_box['classList'][_0x3f6ebd(0x1ce)](_0x3f6ebd(0x1c1)),right_ans_r[_0x3f6ebd(0x1e1)]=right_answers,wrong_ans_r[_0x3f6ebd(0x1e1)]=wrong_answers;const _0x2f4fbb=getid();var _0x13651c=_0x3f6ebd(0x1c5)+_0x3f6ebd(0x19d)+_0x3f6ebd(0x1e6)+_0x3f6ebd(0x1cc)+_0x3f6ebd(0x1ab)+_0x2f4fbb+'&c='+cname,_0x3a4099=_0x3f6ebd(0x1c5)+_0x3f6ebd(0x19d)+_0x3f6ebd(0x1e6)+_0x3f6ebd(0x1cc)+'rtificate%'+'3D'+_0x2f4fbb+'%26c%3D'+cname;document['getElement'+'ById'](_0x3f6ebd(0x1d9))['style']['display']=_0x3f6ebd(0x1e3),document[_0x3f6ebd(0x1ca)+_0x3f6ebd(0x1e0)](_0x3f6ebd(0x1b9))['href']=_0x13651c,document[_0x3f6ebd(0x1ca)+_0x3f6ebd(0x1e0)]('thelink')[_0x3f6ebd(0x1e1)]=_0x13651c,document[_0x3f6ebd(0x1ca)+_0x3f6ebd(0x1e0)]('sharetext')[_0x3f6ebd(0x1e1)]=_0x3f6ebd(0x1cb)+_0x3f6ebd(0x1d8)+_0x3f6ebd(0x1e5)+_0x3f6ebd(0x19e)+'\x20To\x20Linked'+'In',document[_0x3f6ebd(0x1ca)+_0x3f6ebd(0x1e0)](_0x3f6ebd(0x1d6))['innerHTML']=_0x3f6ebd(0x19c)+_0x3f6ebd(0x1a1)+'<a\x20href=\x22h'+_0x3f6ebd(0x1c6)+_0x3f6ebd(0x1df)+'com/in/nar'+_0x3f6ebd(0x1e6)+'di\x22>Narend'+_0x3f6ebd(0x1d4)+'</a>\x20And\x20<'+'a\x20href=\x22ht'+_0x3f6ebd(0x1c7)+_0x3f6ebd(0x1b5)+_0x3f6ebd(0x1c3)+'/narendrad'+_0x3f6ebd(0x19b)+_0x3f6ebd(0x1aa)+_0x3f6ebd(0x1be)+_0x3f6ebd(0x1a6)+'dIn',document[_0x3f6ebd(0x1ca)+'ById'](_0x3f6ebd(0x1bb)+'nk')[_0x3f6ebd(0x1c2)]=linkedin1+_0x3a4099+linkedin2+_0x2f4fbb+encodeURIComponent(_0x3f6ebd(0x1b6))+cname,_0x345103[_0x3f6ebd(0x1c0)]=_0x3f6ebd(0x1d1)+'ibb.co/j80'+_0x3f6ebd(0x1ae)+_0x3f6ebd(0x1d0);const _0x4059a8=_0x2f4fbb,_0x3e377=document[_0x3f6ebd(0x1ca)+'ById'](_0x3f6ebd(0x1db))['value'];var _0x24bc5a=new Date(),_0x3067fd=_0x24bc5a[_0x3f6ebd(0x1e4)+'r']()+'-'+(_0x24bc5a[_0x3f6ebd(0x1c9)]()+0x1)+'-'+_0x24bc5a['getDate'](),_0x277935=_0x24bc5a[_0x3f6ebd(0x1dc)]()+':'+_0x24bc5a['getMinutes']()+':'+_0x24bc5a['getSeconds'](),_0x2378d9=_0x3067fd+'\x20'+_0x277935,_0x1605b9=document[_0x3f6ebd(0x1ca)+'ById'](_0x3f6ebd(0x1a5)),_0xd2b263='';if(_0x1605b9['innerText']='b')_0xd2b263=_0x3f6ebd(0x1a0);else{if(_0x1605b9[_0x3f6ebd(0x1e1)]='i')_0xd2b263=_0x3f6ebd(0x1a9)+'te';else(_0x1605b9[_0x3f6ebd(0x1e1)]='a')?_0x3f6ebd(0x1b3)===_0x3f6ebd(0x1b3)?_0xd2b263=_0x3f6ebd(0x1a7):_0x56c3d3='Advance':_0x3f6ebd(0x1e2)!==_0x3f6ebd(0x1a2)?_0xd2b263=_0x3f6ebd(0x1c8):_0x303e26=_0x3f6ebd(0x1c8);}firebase[_0x3f6ebd(0x1de)]()[_0x3f6ebd(0x1a8)](cname+'/'+_0x4059a8)[_0x3f6ebd(0x1ac)]({'ID':_0x4059a8,'Name':_0x3e377,'Difficulty':_0xd2b263,'Quiz':thetitle,'Date':_0x3067fd,'Time':_0x277935});}}else alert(_0x3f6ebd(0x1b7)+'\x20Is\x20Not\x2045'+_0x3f6ebd(0x1d2)+_0x3f6ebd(0x1c4)+_0x3f6ebd(0x1d5)+_0x3f6ebd(0x1b8)),window['location']=_0x3f6ebd(0x1af)+'iztest.nar'+_0x3f6ebd(0x1e6)+_0x3f6ebd(0x1b2);}asking['length']-0x1==que_index&&(next_btn['innerText']=_0x3f6ebd(0x1ba));});function _0x224a(){var _0x549ff8=['%+\x20,\x20Pleas','pkxkT','ra\x20Dwivedi','After\x20Lear','credit','21292zXBHdt','Button\x20To\x20','certid','length','username','getHours','11520027igDgBL','database','.linkedin.','ById','innerText','Fpvlv','block','getFullYea','Add\x20Your\x20C','endradwive','linkedinim','wivedi\x22>Na','Make\x20Sure\x20','lidate.nar','ertificate','6642695EMeBPm','Beginner','To\x20Follow\x20','PCtPs','8gfCwcJ','add','level','>\x20On\x20Linke','Advance','ref','Intermedia','rendraDwiv','rtificate=','set','onclick','w7tX/linke','https://qu','298667DRtdtU','1701056MCtTtN','di.org','KtStu','8531243KcGniF','linkedin.c','&c=','Your\x20Marks','ning','thelink','Finish','linkedinli','231DZxTpR','6IHCgyW','edi.Org</a','toFixed','src','inactive','href','om/company','e\x20Attempt\x20','https://va','ttps://www','tps://www.','Normal','getMonth','getElement','Use\x20Below\x20','di.org/?ce','classList','remove','17185050VTldjB','din.png','https://i.'];_0x224a=function(){return _0x549ff8;};return _0x224a();}
 
 function UserAnswer(answer){
     let userAns = answer.innerText;
